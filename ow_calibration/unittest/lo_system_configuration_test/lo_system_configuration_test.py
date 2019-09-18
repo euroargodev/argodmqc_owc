608d6103838d8a30a44278da055ed7adf6b496f6
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-09-18 15:16:53 +0100

    Added test to check type of data structure

--- ow_calibration/unittest/lo_system_configuration_test/lo_system_configuration_test.py
+++ ow_calibration/unittest/lo_system_configuration_test/lo_system_configuration_test.py
@@ -4,6 +4,9 @@ from ow_calibration.lo_system_configuration.ow_config import lo_system_configura
 
 class lo_system_configuration_test(unittest.TestCase):
 
+    def test_is_dict(self):
+        self.assertTrue(isinstance(lo_system_configuration,dict))
+
     def test_all_keys_have_value(self):
         for i in lo_system_configuration:
             self.assertNotEqual(lo_system_configuration[i], '')
