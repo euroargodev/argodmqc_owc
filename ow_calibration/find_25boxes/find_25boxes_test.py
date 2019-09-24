f7895a7d032cd016e67d0d0e691466c2ab9f254f
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-09-24 12:24:32 +0100

    Added documentation comments

--- ow_calibration/find_25boxes/find_25boxes_test.py
+++ ow_calibration/find_25boxes/find_25boxes_test.py
@@ -1,3 +1,15 @@
+"""
+-----Load Configuration Test File-----
+
+Written by: Edward Small
+When: 24/09/2019
+
+Contains unit tests to check the functionaility of the `find_25boxes` function
+
+To run this test specifically, look at the documentation at:
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
+
 import unittest
 import scipy.io as scipy
 
