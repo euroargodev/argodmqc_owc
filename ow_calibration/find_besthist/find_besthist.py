69c9ed7469304364a7579d4ee1d5f1b92892205d
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-10-30 14:03:25 +0000

    Added python file for find_besthist function, and created inital comment

---
+++ ow_calibration/find_besthist/find_besthist.py
@@ -0,0 +1,21 @@
+"""
+-----find best historical casts-----
+
+Written by: Breck Owens and Annie Wong
+When: xx/12/2006
+Converted to python by: Edward Small
+When: 30/10/2019
+
+Find ln_max_casts unique historical points that are most strongly correlated with the float profile
+
+Rewritten in December 2006 to only use latitude, longitude, data, and water depth as arguments
+and to return index of the station list
+
+N.B. Change to code on the xx/06/2013: add age_large when computing correlation_large
+- C Cabanes
+
+For information on how to use this file, check the README at either:
+
+https://github.com/ArgoDMQC/matlab_owc
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
