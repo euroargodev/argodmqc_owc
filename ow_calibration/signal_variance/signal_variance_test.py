0f85891f2898e3a02f71f4678a2ec5abe005d2e5
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-11-21 14:10:39 +0000

    Added two new test for signal_variance:
    1) test that answers the same for input and -1 * input
    2) test that weget the expected answer from normal inputs

--- ow_calibration/signal_variance/signal_variance_test.py
+++ ow_calibration/signal_variance/signal_variance_test.py
@@ -47,7 +47,7 @@ class SignalVarianceTestCase(unittest.TestCase):
         Check that we ignore 0's and nan values
         :return: Nothing
         """
-        print("Testing that")
+        print("Testing that signal_variance ignores 0's/NaNs correctly")
 
         expected = signal_variance([1, 2, 3, 4, 5])
         zeroes = signal_variance([1, 0, 2, 0, 3, 0, 4, 0, 5, 0])
@@ -55,6 +55,31 @@ class SignalVarianceTestCase(unittest.TestCase):
 
         self.assertEqual(expected, zeroes, "signal variance is not ignoring 0's")
         self.assertEqual(expected, nans, "sign_variance is not ignoring NaN's")
+
+    def test_negtive_inputs_against_positive(self):
+        """
+        Check that giving a set of negative inputs gives the same result as positive inputs
+        :return: Nothing
+        """
+        print("Testing that signal_variance returns the same result for input and -input")
+
+        positive = signal_variance([30, 20, 10, 40, 50])
+        negative = signal_variance([-30, -20, -10, -40, -50])
+
+        self.assertEqual(positive, negative,
+                         "signal_variance is not returning the same result for input and -input")
+
+    def test_returns_correct_result(self):
+        """
+        Check that we get the expected result from some inputs
+        :return: Nothing
+        """
+        print("Testing that signal_variance gives the expected results")
+
+        expected = 0.185000000000001
+        ans = signal_variance([-35, -35.5, -35.7, -36.2])
+
+        self.assertEqual(ans, expected, "signal_variance did not give the expected result")
 
 
 if __name__ == '__main__':
