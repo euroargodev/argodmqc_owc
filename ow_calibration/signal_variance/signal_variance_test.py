0f982694aa04bba2dcd3da6b122fb6f696f48732
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-11-21 13:54:18 +0000

    Added test to check that 0's and nans are ignored

--- ow_calibration/signal_variance/signal_variance_test.py
+++ ow_calibration/signal_variance/signal_variance_test.py
@@ -10,7 +10,6 @@ To run this test specifically, look at the documentation at:
 https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
 """
 
-
 import unittest
 from .signal_variance import signal_variance
 
@@ -29,6 +28,33 @@ class SignalVarianceTestCase(unittest.TestCase):
 
         var = signal_variance([1, 2, 3, 4, 5])
         self.assertTrue(isinstance(var, float), "signal variance is not a float")
+
+    def test_throws_exception(self):
+        """
+        Check that we thrown an exception if no valid salinities are given
+        :return: Nothing
+        """
+        print("Testing that signal_variance throws an exception for no valid salinities")
+
+        with self.assertRaises(Exception) as no_valid_sal:
+            signal_variance([0, 0, float('nan'), float('nan')])
+
+        self.assertTrue('Received no valid salinity values when calculating signal variance'
+                        in str(no_valid_sal.exception))
+
+    def test_nans_are_ignored(self):
+        """
+        Check that we ignore 0's and nan values
+        :return: Nothing
+        """
+        print("Testing that")
+
+        expected = signal_variance([1, 2, 3, 4, 5])
+        zeroes = signal_variance([1, 0, 2, 0, 3, 0, 4, 0, 5, 0])
+        nans = signal_variance([1, 2, 3, 4, 5, float('nan'), float('nan')])
+
+        self.assertEqual(expected, zeroes, "signal variance is not ignoring 0's")
+        self.assertEqual(expected, nans, "sign_variance is not ignoring NaN's")
 
 
 if __name__ == '__main__':
