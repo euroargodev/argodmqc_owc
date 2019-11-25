a985f691af589da6dc72c5de4730b215936d6dc6
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-11-25 12:44:49 +0000

    Added Test file for map_data_grid

---
+++ ow_calibration/map_data_grid/map_data_grid_test.py
@@ -0,0 +1,25 @@
+"""
+-----Map Data to Grid Test File-----
+
+Written by: Edward Small
+When: 24/09/2019
+
+Contains unit tests to check the functionality of the `map_data_grid` function
+
+To run this test specifically, look at the documentation at:
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
+
+import unittest
+
+
+class MapDataGridTestCase(unittest.TestCase):
+    """
+    Test cases for map_data_grid function
+    """
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
