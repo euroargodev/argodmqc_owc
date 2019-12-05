930490ec4aacaaa6ad14bac71de2af8a2ddab758
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2019-12-05 08:42:05 +0000

    Reset data array when we have added the currently selected data
    This prevents from adding duplicated data

--- ow_calibration/get_region/get_region_hist_locations.py
+++ ow_calibration/get_region/get_region_hist_locations.py
@@ -1,20 +1,44 @@
-from ow_calibration.find_25boxes.find_25boxes import find_25boxes
+"""
+-----Get Historical Region Data-----
+
+Written by: Breck Owens
+When: xx/12/2006
+Converted to python by: Edward Small
+When: 05/12/2019
+
+Function returns the needed spatial and temporal data from each of the historical
+data points inside the WMO boxes it is given, excluding the current float being processed.
+The WMO boxes are passed in the form of [WMO name, use ctd data, use bottle data, use argo data],
+and the configuration is also passed in, which is used to locate the different data locally. The
+configuration can be changed in load_configuration.py
+
+N.B. Change to code on xx/xx/2015 - Only load longitude, latitude, and dates
+
+For information on how to use this file, check the README at either:
+
+https://github.com/ArgoDMQC/matlab_owc
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
+
 from ow_calibration.load_configuration.load_configuration import load_configuration
 from ow_calibration.change_dates.change_dates import change_dates
 import numpy as np
 import scipy.io as scipy
 
-config = load_configuration()
-pa_wmo_boxes = scipy.loadmat('../../data/constants/wmo_boxes.mat')
-pn_float_long = 57.1794
-pn_float_lat = -59.1868
 
-boxes = find_25boxes(pn_float_long, pn_float_lat, pa_wmo_boxes)
-
-
-def get_region_hist_locations(pa_wmo_numbers, pa_float_name, pa_config_data):
+def get_region_hist_locations(pa_wmo_numbers, pa_float_name, config):
+    """
+    Uses the WMO boxes and to return all of the historical data in the given area,
+    excluding the float that is currently being analysed.
+    :param pa_wmo_numbers: 2D array containing the name of the WMO boxes that cover the area
+    of interest, and flags for whether we want to use argo, bottle, and/or CTD data
+    :param pa_float_name: string of the name of the float currently being processed
+    :param config: Dictionary containing configuration settings. Used to find locations of folders
+    and file containing data
+    :return: the latitude, longitude and age of each data point we want
+    """
     # set up matrices to hold data
-    grid_lat = np.array([])
+    grid_lat = []
     grid_long = []
     grid_dates = []
     not_use = []
@@ -48,11 +72,12 @@ def get_region_hist_locations(pa_wmo_numbers, pa_float_name, pa_config_data):
                 data['long'] = [np.delete(data['long'], not_use)]
                 data['dates'] = [np.delete(data['dates'], not_use)]
 
-            # if we have data, combine it with the other data
+            # if we have data, combine it with the other data then reset it
             if data:
                 grid_lat = np.concatenate([grid_lat, data['lat'][0]])
                 grid_long = np.concatenate([grid_long, data['long'][0]])
                 grid_dates = np.concatenate([grid_dates, data['dates'][0]])
+                data = []
 
     if grid_lat.__len__() == 0:
         grid_lat = 999
@@ -73,12 +98,13 @@ def get_region_hist_locations(pa_wmo_numbers, pa_float_name, pa_config_data):
     # decimalise dates
     grid_dates = change_dates(grid_dates)
 
-    for i in range(0, grid_lat.__len__()):
-        print("historial data: ", grid_lat[i], " ", grid_long[i], " ", grid_dates[i])
-
     return grid_lat, grid_long, grid_dates
 
 
-boxes = np.array([[3505, 1, 1, 1]])
+config = load_configuration()
 
-get_region_hist_locations(boxes, '1900193', 1)
+boxes = np.array([[3505, 1, 0, 0]])
+
+lat, long, age = get_region_hist_locations(boxes, '1900193', config)
+
+print(lat.__len__())
