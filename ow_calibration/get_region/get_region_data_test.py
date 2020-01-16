4101d3bb507cf894206f19ae063dd6092e469fba
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2020-01-16 14:12:19 +0000

    Reformated file

--- ow_calibration/get_region/get_region_data_test.py
+++ ow_calibration/get_region/get_region_data_test.py
@@ -14,21 +14,21 @@ import unittest
 import numpy as np
 from ow_calibration.get_region.get_region_data import get_region_data
 
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         wmo = np.array([[3505, 1, 0, 0],
-                  [3506, 1, 0, 0]])
+                        [3506, 1, 0, 0]])
         float_name = "3901960"
         index = np.array([1, 5, 6, 12, 14])
         pres = np.array([3, 5, 15.1, 25.1, 36])
-        config = config = {'HISTORICAL_DIRECTORY': "data/climatology",
-                       'HISTORICAL_CTD_PREFIX': "/historical_ctd/ctd_",
-                       'HISTORICAL_BOTTLE_PREFIX': "/historical_bot/bot_",
-                       'HISTORICAL_ARGO_PREFIX': "/historical_argo/argo_",
+        config = {'HISTORICAL_DIRECTORY': "data/climatology",
+                           'HISTORICAL_CTD_PREFIX': "/historical_ctd/ctd_",
+                           'HISTORICAL_BOTTLE_PREFIX': "/historical_bot/bot_",
+                           'HISTORICAL_ARGO_PREFIX': "/historical_argo/argo_",
                            'MAP_P_DELTA': 250}
 
         get_region_data(wmo, float_name, config, index, pres)
-
 
 
 if __name__ == '__main__':
