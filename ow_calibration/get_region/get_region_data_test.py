673ef53d19c425572e25e1048a1d882084a27b52
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2020-01-16 13:39:11 +0000

    Edited test file to check outputs from get_region_data

--- ow_calibration/get_region/get_region_data_test.py
+++ ow_calibration/get_region/get_region_data_test.py
@@ -12,11 +12,20 @@ https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
 
 import unittest
 import numpy as np
-from ow_calibration.get_region.get_region_hist_locations import get_region_hist_locations
+from ow_calibration.get_region.get_region_data import get_region_data
+from ow_calibration.load_configuration.load_configuration import load_configuration
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        wmo = np.array([[3505, 1, 0, 0],
+                  [3506, 1, 0, 0]])
+        float_name = "3901960"
+        index = np.array([1, 5, 6, 12, 14])
+        pres = np.array([3, 5, 15.1, 25.1, 36])
+        config = load_configuration()
+
+        get_region_data(wmo, float_name, config, index, pres)
+
 
 
 if __name__ == '__main__':
