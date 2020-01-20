60809fd42b7309a49d21f42673b17ba5c605d9e1
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2020-01-20 11:35:30 +0000

    Find the minimum difference between climitology data and float data

--- ow_calibration/interp_climatology/interp_climatology.py
+++ ow_calibration/interp_climatology/interp_climatology.py
@@ -41,22 +41,22 @@ def interp_climatology(grid_sal, grid_theta, grid_pres, float_sal, float_theta, 
     max_level = 0
     for n in range(0, grid_station):
         # find where data is not missing
-        good_sal = np.isfinite(grid_sal[:, n])
-        good_theta = np.isfinite(grid_theta[:, n])
-        good_pres = np.isfinite(grid_pres[:, n])
+        grid_good_sal = np.isfinite(grid_sal[:, n])
+        grid_good_theta = np.isfinite(grid_theta[:, n])
+        grid_good_pres = np.isfinite(grid_pres[:, n])
 
         # find indices of good data
-        good_data_index = (good_sal == good_theta)
-        good_data_index = np.argwhere(good_data_index == good_pres)
+        grid_good_data_index = (grid_good_sal == grid_good_theta)
+        grid_good_data_index = np.argwhere(grid_good_data_index == grid_good_pres)
 
         # now find the max level
-        good_data_len = good_data_index.__len__()
-        if good_data_len != 0:
-            for m in range(0, good_data_len):
-                grid_sal[m, n] = grid_sal[good_data_index[m], n]
-                grid_theta[m, n] = grid_theta[good_data_index[m], n]
-                grid_pres[m, n] = grid_pres[good_data_index[m], n]
-            max_level = np.maximum(max_level, good_data_len)
+        grid_good_data_len = grid_good_data_index.__len__()
+        if grid_good_data_len != 0:
+            for m in range(0, grid_good_data_len):
+                grid_sal[m, n] = grid_sal[grid_good_data_index[m], n]
+                grid_theta[m, n] = grid_theta[grid_good_data_index[m], n]
+                grid_pres[m, n] = grid_pres[grid_good_data_index[m], n]
+            max_level = np.maximum(max_level, grid_good_data_len)
 
     # Truncate the number of levels to the maximum level that has
     # available data
@@ -66,3 +66,41 @@ def interp_climatology(grid_sal, grid_theta, grid_pres, float_sal, float_theta, 
         grid_pres = grid_pres[:max_level, :]
     else:
         raise ValueError("No good climatological data has been found")
+
+    # find where data isn't missing in the float
+    float_good_sal = np.isfinite(float_sal)
+    float_good_theta = np.isfinite(float_theta)
+    float_good_pres = np.isfinite(float_pres)
+
+    # get the indices of the good float data
+    float_good_data_index = (float_good_sal == float_good_theta)
+    float_good_data_index = np.argwhere(float_good_data_index == float_good_pres)
+
+    # get the number of good float data observations
+    float_data_len = float_good_data_index.__len__()
+
+    # compare good float data to the closest climatological data
+    for n in range(0, float_data_len):
+
+        # get index of good float data
+        index = float_good_data_index[n]
+
+        # Find the difference between the float data and the climatological data
+        delta_sal = grid_sal - float_sal[index]
+        delta_pres = grid_pres - float_pres[index]
+        delta_theta = grid_theta - float_theta[index]
+
+        # Find the closest pressure value
+        delta_pres_abs = np.abs(delta_pres)
+        delta_pres_min = np.argwhere(delta_pres_abs.min(axis=0) == delta_pres_abs)[:, 0]
+        print(delta_pres_min)
+
+        # go through all the climatological stations
+        for m in range(0, grid_station):
+
+            # find if delta_theta is different to delta_theta for closest pressure
+            tst = np.sign(delta_theta[:,m]) * np.sign(delta_theta[delta_pres_min[n], n])
+
+
+
+
