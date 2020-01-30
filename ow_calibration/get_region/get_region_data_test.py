20bd1c90d63d0f8b4b4d77d65276dc155d59b3ac
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2020-01-30 14:01:15 +0000

    Test to check get_region_data returns 6 arrays

--- ow_calibration/get_region/get_region_data_test.py
+++ ow_calibration/get_region/get_region_data_test.py
@@ -16,23 +16,29 @@ from ow_calibration.get_region.get_region_data import get_region_data
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        wmo = np.array([[3505, 1, 1, 1],
+    def setUp(self):
+        self.wmo_boxes = np.array([[3505, 1, 1, 1],
                         [3506, 1, 1, 1]])
-        float_name = "3901960"
-        float_name = "1900479" # should be removed
-        index = np.array([0, 4, 5, 11, 13])
-        pres = np.array([3, 5, 15.1, 25.1, 36])
-        config = {'HISTORICAL_DIRECTORY': "data/climatology",
+        self.float_name = "3901960"
+        self.float_name_removed = "1900479"
+        self.index = np.array([0, 4, 5, 11, 13, 15, 20, 21, 42])
+        self.pres = np.array([3, 5, 15.1, 25.1, 36, 40, 45, 46, 500000])
+        self.config = {'HISTORICAL_DIRECTORY': "data/climatology",
                   'HISTORICAL_CTD_PREFIX': "/historical_ctd/ctd_",
                   'HISTORICAL_BOTTLE_PREFIX': "/historical_bot/bot_",
                   'HISTORICAL_ARGO_PREFIX': "/historical_argo/argo_",
                   'MAP_P_DELTA': 250}
 
-        test = get_region_data(wmo, float_name, config, index, pres)
+    def test_returns_6(self):
+        """
+        check that the function returns 6 arrays
+        :return: Nothing
+        """
+        print("Testing that get_region_data returns 6 arrays")
+        test = get_region_data(self.wmo_boxes, self.float_name, self.config,
+                               self.index, self.pres)
 
-        print(test[4])
-
+        self.assertTrue(test.__len__() == 6, "Should return 6 arrays")
 
 if __name__ == '__main__':
     unittest.main()
