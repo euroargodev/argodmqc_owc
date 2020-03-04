d432086520e9ef88f52ded1150f44eec8ae69fef
Edward Small <edsmall@livljobs7.nerc-liv.ac.uk>
2020-03-04 09:16:42 +0000

    Added file to run function for testing

---
+++ ow_calibration/update_salinity_mapping/update_salinity_mapping_test.py
@@ -0,0 +1,12 @@
+import unittest
+from ow_calibration.load_configuration.load_configuration import load_configuration
+from ow_calibration.update_salinity_mapping.update_salinity_mapping import update_salinity_mapping
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        update_salinity_mapping("hello/", "1111", load_configuration())
+
+
+if __name__ == '__main__':
+    unittest.main()
