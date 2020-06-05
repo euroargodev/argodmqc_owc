9ec4494282ea6c8e5e2a73e4000672d2f39bfbdd
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-06-05 09:28:41 +0100

    Documentation for sorter

---
+++ ow_calibration/sorter/sorter.py
@@ -0,0 +1,15 @@
+"""
+-----Sorter Function-----
+
+Written by: Edward Small
+When: 05/06/2020
+
+Function to fill out arrays for the piecewise-fit
+
+Used to find the interval in a linear fit
+
+For information on how to use this file, check the README at either:
+
+https://github.com/ArgoDMQC/matlab_owc
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
