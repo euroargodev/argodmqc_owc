7ed169b2d82dac79a1c81aa14087686b2967ea33
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-06-05 12:36:04 +0100

    Flag values as positive (1) or negative (0)

--- ow_calibration/sorter/sorter.py
+++ ow_calibration/sorter/sorter.py
@@ -16,6 +16,7 @@ https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
 
 import numpy as np
 
+
 def sorter(msites, sites):
     """
     Flag points as negative or positve
@@ -24,4 +25,11 @@ def sorter(msites, sites):
     :return: Array containing values defining points as positive or negative
     """
 
-    all_sites = np.concatenate(msites.flatten(), sites.flatten())
+    # put arrays together
+    all_sites = np.concatenate((msites.flatten(), sites.flatten()))
+
+    index = np.argsort(all_sites)
+
+    pointer = np.argwhere(index > msites.__len__() - 1).flatten() - np.arange(1, sites.__len__() + 1)
+
+    return pointer
--- ow_calibration/sorter/sorter_test.py
+++ ow_calibration/sorter/sorter_test.py
@@ -1,9 +1,15 @@
 import unittest
-
+import numpy as np
+from ow_calibration.sorter.sorter import sorter
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+
+        A = np.array([-1, 0])
+        B = np.arange(-1, 1.01, 0.01)
+
+        sorter(A, B)
+
 
 
 if __name__ == '__main__':
