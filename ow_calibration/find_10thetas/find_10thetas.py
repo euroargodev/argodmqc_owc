48b9056671edf9c3cf6adc2b20ef9b440a56bdc8
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-06-11 13:14:54 +0100

    Set up arrays

--- ow_calibration/find_10thetas/find_10thetas.py
+++ ow_calibration/find_10thetas/find_10thetas.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def find_10thetas(SAL, PTMP, PRES, la_ptmp,
                   use_theta_lt, use_theta_gt,
@@ -17,4 +17,18 @@ def find_10thetas(SAL, PTMP, PRES, la_ptmp,
     :return: Theta levels where salinity varies the least
     """
 
-print("hello world")
+    # We only want 10 theta levels
+    no_levels = 10
+
+    # Find how much data we will need to go through
+    profile_no = PRES.shape[0]
+    profile_depth = PRES.shape[1]
+
+    # arrays to hold indices with lowest variance and levels
+    index = np.empty((no_levels, profile_depth)) * np.nan
+    t_levels = np.empty((no_levels, 1)) * np.nan
+    p_levels = np.empty((no_levels, 1)) * np.nan
+    var_sal_theta = []
+    theta_levels = []
+
+
