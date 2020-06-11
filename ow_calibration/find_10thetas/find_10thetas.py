509a28465332d12178ee06e0bce47d4bed90c48d
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-06-11 12:58:46 +0100

    Function documentation

---
+++ ow_calibration/find_10thetas/find_10thetas.py
@@ -0,0 +1,18 @@
+
+
+def find_10thetas(SAL, PTMP, PRES, la_ptmp,
+                  use_theta_lt, use_theta_gt,
+                  use_pres_lt, use_pres_gt, use_percent_gt):
+    """
+    Find on which theta levels salinity variance is lowest
+    :param SAL: float salinity
+    :param PTMP: float potential temperature
+    :param PRES: float pressure
+    :param la_ptmp: mapped potential temperature
+    :param use_theta_lt: lower bound for potential temperature
+    :param use_theta_gt: upper bound for potential temperature
+    :param use_pres_lt: lower bound for pressure
+    :param use_pres_gt: upper bound for pressure
+    :param use_percent_gt: use percentage greater than
+    :return: Theta levels where salinity varies the least
+    """
