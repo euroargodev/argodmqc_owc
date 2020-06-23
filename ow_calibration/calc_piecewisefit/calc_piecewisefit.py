35eade23a11018670267ad6bab29f0d8d404c023
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-06-23 16:59:29 +0100

    Find depth values and create XYZ matrix

--- ow_calibration/calc_piecewisefit/calc_piecewisefit.py
+++ ow_calibration/calc_piecewisefit/calc_piecewisefit.py
@@ -12,8 +12,12 @@ https://github.com/ArgoDMQC/matlab_owc
 https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
 """
 
+import copy
 import numpy as np
 import scipy.io as scipy
+import scipy.interpolate as interpolate
+from ow_calibration.tbase_decoder.tbase_decoder import get_topo_grid
+
 
 def calc_piecewisefit(float_dir, float_name, system_config):
     """
@@ -36,7 +40,7 @@ def calc_piecewisefit(float_dir, float_name, system_config):
     ptmp = float_source_data['PTMP']
     pres = float_source_data['PTMP']
     profile_no = float_source_data['PROFILE_NO']
-    x_in = np.tile(profile_no, (10,1))
+    x_in = np.tile(profile_no, (10, 1))
 
     # load in the mapped data
     float_mapped_data = scipy.loadmat(system_config['FLOAT_MAPPED_DIRECTORY'] +
@@ -46,3 +50,34 @@ def calc_piecewisefit(float_dir, float_name, system_config):
     mapped_sal = float_mapped_data['la_mapped_sal']
     mapsalerror = float_mapped_data['la_mapsalerrors']
     mapped_ptmp = float_mapped_data['la_ptmp']
+    selected_hist = float_mapped_data['selected_hist']
+
+    # retrieve XYZ of float position used to build covariance
+    if selected_hist.__len__() > 0:
+        if long.shape[0] > 1:
+            long = long.flatten()
+
+        if lat.shape[0] > 1:
+            lat = lat.flatten()
+
+        if np.any(long > 180):
+            long_1 = copy.deepcopy(long) - 360
+
+        else:
+            long_1 = copy.deepcopy(long)
+
+        elev, x_grid, y_grid = get_topo_grid(np.nanmin(long_1) - 1, np.nanmax(long_1) + 1,
+                                             np.nanmin(lat) - 1, np.nanmax(lat) + 1)
+
+        grid_interp = interpolate.interp2d(x_grid[0, :], y_grid[:, 0],
+                                           elev, kind='linear')
+
+        z_grid = []
+        for i in range(long_1[0].__len__()):
+            z_grid.append(grid_interp(long_1[0][i], lat[0][i]))
+
+        z_grid = -np.array(z_grid)
+
+        coord_float = np.column_stack((long.T, lat.T, z_grid))
+
+        print(coord_float)
