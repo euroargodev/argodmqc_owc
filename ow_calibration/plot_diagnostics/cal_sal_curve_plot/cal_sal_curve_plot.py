2fb80f312f413b6d5732b85bf8711a65fec8f1b3
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-07-05 17:06:01 +0100

    Set up function and test file

---
+++ ow_calibration/plot_diagnostics/cal_sal_curve_plot/cal_sal_curve_plot.py
@@ -0,0 +1,23 @@
+"""
+-----Theta Salinity Plot-----
+
+Written by: Edward Small
+When: 10/05/2020
+
+Function for plotting the calibrated salinity curve
+
+For information on how to use this file, check the README at either:
+https://github.com/ArgoDMQC/matlab_owc
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import numpy as np
+
+
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=no-member
+def cal_sal_curve_plot():
+    print("called")
---
+++ ow_calibration/plot_diagnostics/cal_sal_curve_plot/cal_sal_curve_plot_test.py
@@ -0,0 +1,65 @@
+"""
+-----Theta Salinity Curve Test File-----
+Written by: Edward Small
+When: 11/05/2019
+Contains unit tests to check the functionality of the `trajectory_plot` function
+To run this test specifically, look at the documentation at:
+https://gitlab.noc.soton.ac.uk/edsmall/bodc-dmqc-python
+"""
+
+import copy
+import unittest
+from unittest.mock import patch
+import scipy.io as scipy
+import numpy as np
+from ow_calibration.find_10thetas.find_10thetas import find_10thetas
+from ow_calibration.plot_diagnostics.cal_sal_curve_plot.cal_sal_curve_plot import cal_sal_curve_plot
+
+
+# pylint: disable=bare-except
+# pylint: disable=unused-argument
+class MyTestCase(unittest.TestCase):
+    """
+    Test cases for theta_sal_plot function
+    """
+
+    #@patch("ow_calibration.plot_diagnostics.trajectory_plot.trajectory_plot.plt.show")
+    #def test_plot_runs(self, mockshow):
+    def test_plot_runs(self):
+        """
+        Check we get no errors during the plotting routine
+        :return: nothing
+        """
+
+        print("Test that calibrated salinity curve plot throws no errors")
+
+        cal_sal_curve_plot()
+
+        """grid_data = scipy.loadmat("data/test_data/float_mapped_test/map_3901960.mat")
+        float_data = scipy.loadmat("data/float_source/3901960.mat")
+
+        sal = np.array(float_data['SAL'])
+        theta = np.array(float_data['PTMP'])
+        pres = float_data['PRES']
+        grid_sal = grid_data['la_mapped_sal']
+        grid_ptmp = grid_data['la_ptmp']
+        grid_errors = grid_data['la_mapsalerrors']
+
+        thetas = find_10thetas(copy.deepcopy(sal),
+                               copy.deepcopy(theta),
+                               copy.deepcopy(pres),
+                               copy.deepcopy(grid_ptmp),
+                               0, 0,
+                               0, 0,
+                               0.5)
+
+        index = thetas[2]
+
+        # Check various types run
+
+        self.assertEqual(theta_sal_plot(sal.transpose(), theta.transpose(),
+                                        grid_sal, grid_ptmp, grid_errors, index), None)
+
+"""
+if __name__ == '__main__':
+    unittest.main()
