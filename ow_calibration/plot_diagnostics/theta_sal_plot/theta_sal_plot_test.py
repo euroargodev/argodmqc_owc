f82ed3d174071846ae661db16006d40bc12c5e61
Edward Small <edsmall@livljobs8.nerc-liv.ac.uk>
2020-07-06 13:51:28 +0100

    Test doesn't show plot

--- ow_calibration/plot_diagnostics/theta_sal_plot/theta_sal_plot_test.py
+++ ow_calibration/plot_diagnostics/theta_sal_plot/theta_sal_plot_test.py
@@ -25,9 +25,8 @@ class MyTestCase(unittest.TestCase):
     Test cases for theta_sal_plot function
     """
 
-    # @patch("ow_calibration.plot_diagnostics.trajectory_plot.trajectory_plot.plt.show")
-    # def test_plot_runs(self, mockshow):
-    def test_plot_runs(self):
+    @patch("ow_calibration.plot_diagnostics.trajectory_plot.trajectory_plot.plt.show")
+    def test_plot_runs(self, mockshow):
         """
         Check we get no errors during the plotting routine
         :return: nothing
@@ -58,8 +57,9 @@ class MyTestCase(unittest.TestCase):
 
         # Check various types run
 
-        theta_sal_plot(sal.transpose(), theta.transpose(),
-                       map_sal, map_ptmp, map_errors, index, profiles)
+        self.assertEqual(theta_sal_plot(sal.transpose(), theta.transpose(),
+                                        map_sal, map_ptmp, map_errors, index, profiles),
+                         None)
 
 
 if __name__ == '__main__':
