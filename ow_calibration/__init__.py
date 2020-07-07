78c0e861c5ffb1af812cf52f4f356ca1dc307fb2
Guillaume Maze <maze.guillaume@gmail.com>
2020-07-07 09:40:36 +0200

    Update __init__.py

--- ow_calibration/__init__.py
+++ ow_calibration/__init__.py
@@ -4,8 +4,8 @@ Global import controler
 
 try:
     import pkg_resources
-    __version__ = pkg_resources.get_distribution("py_owc").version
-except Exception:
+    __version__ = pkg_resources.get_distribution("pyowc").version
+except ImportError:
     # Local copy, not installed with setuptools, or setuptools is not available.
     # Disable minimum version checks on downstream libraries.
     __version__ = "999"
