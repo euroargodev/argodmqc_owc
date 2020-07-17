f21e2440f8ec36afbdb3baebe3bf03a39d2a03de
edsmall-bodc <edsmall@bodc.ac.uk>
2020-07-17 16:21:10 +0100

    Now has 32 keys!

--- pyowc/tests/configuration_test.py
+++ pyowc/tests/configuration_test.py
@@ -40,7 +40,7 @@ class LoadConfiguration(unittest.TestCase):
         :return: Nothing
         """
         print("Testing length of dictionary...\n")
-        self.assertEqual(self.lo_system_configuration.__len__(), 31)
+        self.assertEqual(self.lo_system_configuration.__len__(), 32)
 
     def test_all_keys_have_value(self):
         """
