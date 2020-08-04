58cfa5629ce1ea3291ba821f798286dea48bade8
edsmall-bodc <edsmall@bodc.ac.uk>
2020-08-04 12:58:30 +0100

    Salinity mapping loop

--- start_with_pycharm.py
+++ start_with_pycharm.py
@@ -2,12 +2,18 @@ from argodmqc_owc import pyowc as owc
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-FLOAT_NAME = "3901960"
+FLOAT_NAMES = ["3901960", "OTHER FLOAT"] # add float numbers here
 USER_CONFIG = owc.configuration.load() # fetch the default configuration and parameters
 USER_CONFIG['MAP_USE_PV'] = 0 # Possibly tune options
 print(owc.configuration.print_cfg(USER_CONFIG))
 
-owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
-owc.calibration.update_salinity_mapping("/", FLOAT_NAME, USER_CONFIG)
-owc.calibration.calc_piecewisefit("/", FLOAT_NAME, USER_CONFIG)
-owc.dashboard("/", FLOAT_NAME, USER_CONFIG)
+#owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
+
+# only need salinity mapping
+
+for float in FLOAT_NAMES:
+    owc.calibration.update_salinity_mapping("/", float, USER_CONFIG)
+
+
+#owc.calibration.calc_piecewisefit("/", FLOAT_NAME, USER_CONFIG)
+#owc.dashboard("/", FLOAT_NAME, USER_CONFIG)
