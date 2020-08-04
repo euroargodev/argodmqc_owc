13e7663fa1cf151830c58bf055d2b522608f4d89
edsmall-bodc <edsmall@bodc.ac.uk>
2020-08-04 13:06:37 +0100

    Use multiprocessing

--- start_with_pycharm.py
+++ start_with_pycharm.py
@@ -1,13 +1,21 @@
 from argodmqc_owc import pyowc as owc
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
+import multiprocessing
+from functools import partial
 
-FLOAT_NAME = "3901960"
+FLOAT_NAMES = ["3901960", "FLOAT NAME"] # add float names here
 USER_CONFIG = owc.configuration.load() # fetch the default configuration and parameters
 USER_CONFIG['MAP_USE_PV'] = 0 # Possibly tune options
 print(owc.configuration.print_cfg(USER_CONFIG))
 
-owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
-owc.calibration.update_salinity_mapping("/", FLOAT_NAME, USER_CONFIG)
-owc.calibration.calc_piecewisefit("/", FLOAT_NAME, USER_CONFIG)
-owc.dashboard("/", FLOAT_NAME, USER_CONFIG)
+#owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
+
+pool = multiprocessing.Pool(FLOAT_NAMES.__len__())
+func = partial(owc.calibration.update_salinity_mapping, "/", USER_CONFIG)
+pool.map(func, FLOAT_NAMES)
+pool.close()
+pool.join()
+
+#owc.calibration.calc_piecewisefit("/", FLOAT_NAME, USER_CONFIG)
+#owc.dashboard("/", FLOAT_NAME, USER_CONFIG)
