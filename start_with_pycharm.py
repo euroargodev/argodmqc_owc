5b90266601db1e823f6682fd22e584cf3293a69e
edsmall-bodc <edsmall@bodc.ac.uk>
2020-08-04 13:07:19 +0100

    Use maximum of cores - 1

--- start_with_pycharm.py
+++ start_with_pycharm.py
@@ -11,7 +11,7 @@ print(owc.configuration.print_cfg(USER_CONFIG))
 
 #owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
 
-pool = multiprocessing.Pool(FLOAT_NAMES.__len__())
+pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
 func = partial(owc.calibration.update_salinity_mapping, "/", USER_CONFIG)
 pool.map(func, FLOAT_NAMES)
 pool.close()
