0db62165c168d790bd7fc2d24430a55db844e889
edsmall-bodc <edsmall@bodc.ac.uk>
2020-08-04 15:10:37 +0100

    Added timer

--- start_with_pycharm.py
+++ start_with_pycharm.py
@@ -3,19 +3,26 @@ import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 import multiprocessing
 from functools import partial
+import time
 
-FLOAT_NAMES = ["3901960", "FLOAT NAME"] # add float names here
+FLOAT_NAMES = ["3901960", "3901960"] # add float names here
 USER_CONFIG = owc.configuration.load() # fetch the default configuration and parameters
 USER_CONFIG['MAP_USE_PV'] = 0 # Possibly tune options
 print(owc.configuration.print_cfg(USER_CONFIG))
 
 #owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
 
+start = time.time()
+
 pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
 func = partial(owc.calibration.update_salinity_mapping, "/", USER_CONFIG)
 pool.map(func, FLOAT_NAMES)
 pool.close()
 pool.join()
+
+end = time.time()
+
+print("\nTOTAL TIME ELAPSED: ", end - start)
 
 #owc.calibration.calc_piecewisefit("/", FLOAT_NAME, USER_CONFIG)
 #owc.dashboard("/", FLOAT_NAME, USER_CONFIG)
