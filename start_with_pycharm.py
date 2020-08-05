6b612a1de51443a2b38253c9b4cd14d3c9c80f96
edsmall-bodc <edsmall@bodc.ac.uk>
2020-08-05 18:57:05 +0100

    insert `if __name__` guard
    avoids creating recursive subprocesses

--- start_with_pycharm.py
+++ start_with_pycharm.py
@@ -5,24 +5,26 @@ import multiprocessing
 from functools import partial
 import time
 
-FLOAT_NAMES = ["3901960", "3901960"] # add float names here
-USER_CONFIG = owc.configuration.load() # fetch the default configuration and parameters
-USER_CONFIG['MAP_USE_PV'] = 0 # Possibly tune options
-print(owc.configuration.print_cfg(USER_CONFIG))
+if __name__ == '__main__':
 
-#owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
+    FLOAT_NAMES = ["3901960", "3901960"] # add float names here
+    USER_CONFIG = owc.configuration.load() # fetch the default configuration and parameters
+    USER_CONFIG['MAP_USE_PV'] = 0 # Possibly tune options
+    print(owc.configuration.print_cfg(USER_CONFIG))
 
-start = time.time()
+    #owc.configuration.set_calseries("/", FLOAT_NAME, USER_CONFIG)
 
-pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
-func = partial(owc.calibration.update_salinity_mapping, "/", USER_CONFIG)
-pool.map(func, FLOAT_NAMES)
-pool.close()
-pool.join()
+    start = time.time()
 
-end = time.time()
+    pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
+    func = partial(owc.calibration.update_salinity_mapping, "/", USER_CONFIG)
+    pool.map(func, FLOAT_NAMES)
+    pool.close()
+    pool.join()
 
-print("\nTOTAL TIME ELAPSED: ", end - start)
+    end = time.time()
+
+    print("\nTOTAL TIME ELAPSED: ", end - start)
 
 #owc.calibration.calc_piecewisefit("/", FLOAT_NAME, USER_CONFIG)
 #owc.dashboard("/", FLOAT_NAME, USER_CONFIG)
